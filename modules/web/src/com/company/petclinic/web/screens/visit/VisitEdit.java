19062f1647c40b62ee5d28f9f7c49e5d69d458ae
Andrey Trusov <trusov@haulmont.com>
2020-06-01 19:36:37 +0400

    Add Visit Service

--- modules/web/src/com/company/petclinic/web/screens/visit/VisitEdit.java
+++ modules/web/src/com/company/petclinic/web/screens/visit/VisitEdit.java
@@ -1,11 +1,41 @@
 package com.company.petclinic.web.screens.visit;
 
+import com.company.petclinic.entity.Consumable;
+import com.company.petclinic.service.VisitService;
+import com.haulmont.cuba.gui.model.CollectionChangeType;
+import com.haulmont.cuba.gui.model.CollectionContainer;
+import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.petclinic.entity.Visit;
+
+import javax.inject.Inject;
 
 @UiController("petclinic_Visit.edit")
 @UiDescriptor("visit-edit.xml")
 @EditedEntityContainer("visitDc")
 @LoadDataBeforeShow
 public class VisitEdit extends StandardEditor<Visit> {
+
+    @Inject
+    private VisitService visitService;
+
+    @Subscribe(id = "visitDc", target = Target.DATA_CONTAINER)
+    public void onVisitDcItemPropertyChange(InstanceContainer.ItemPropertyChangeEvent<Visit> event) {
+        if ("hoursSpent".equals(event.getProperty())) {
+            refreshAmount();
+        }
+    }
+
+    @Subscribe(id = "consumablesDc", target = Target.DATA_CONTAINER)
+    public void onConsumablesDcCollectionChange(CollectionContainer.CollectionChangeEvent<Consumable> event) {
+        if (event.getChangeType() != CollectionChangeType.REFRESH) {
+            refreshAmount();
+        }
+    }
+
+
+    private void refreshAmount() {
+        Visit visit = getEditedEntity();
+        visit.setAmount(visitService.calculateAmount(visit));
+    }
 }
